c7b68a4ebceccc9d19c44575c92a2bab24932bf2
E4RTTH <kavin.sin@ascendcorp.com>
2021-09-08 22:02:18 +0700

    new code

--- Quiz 01 - Word count & TF-IDF/Word count/read_write_Harry.py
+++ Quiz 01 - Word count & TF-IDF/Word count/read_write_Harry.py
@@ -3,7 +3,7 @@ import time
 with open('Harry Potter.txt','r',encoding="utf-8") as f:
     read = f.readlines()
 
-file_source = "C:\my_config\/file-source-harry.txt"
+file_source = "C:\my_config\/file-source.txt"
 
 for i in range(7,100):
     if read[i] != "\n":
